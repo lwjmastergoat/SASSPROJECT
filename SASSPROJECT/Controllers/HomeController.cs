1566fef8000e668d4e8e798fe36832d6c89652dd
VID\lass4479 <lwjmarketing@outlook.com>
2018-02-01 10:17:10 +0100

    Add project files.

---
+++ SASSPROJECT/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SASSPROJECT.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SASSPROJECT/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SASSPROJECT.MvcApplication" Language="C#" %>
